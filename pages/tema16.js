ce0ca24e90d7e04c504175ff778d6861801de069
ImaMos01 <imoscosoa@unsa.edu.pe>
2021-11-29 09:39:41 -0500

    Hermitiano

--- pages/tema16.js
+++ pages/tema16.js
@@ -26,42 +26,15 @@ let expr7_2 = "$v_1=(-2,1) \\text{ y } v_2=(1,2)$";
 let expr7_3 = "$\\gamma = {v1,v2}$";
 let expr7_4 = "$\\gamma^* = {v1,v2}$";
 let expr7_5="$$X_1=\\frac{1}{\\sqrt{5}} \\begin{bmatrix}  -2 \\\\ 1 \\\\ \\end{bmatrix}"+
-" \\text{ y } X_2 =frac{1}{\\sqrt{5}} \\begin{bmatrix}  1 \\\\ 2 \\\\ \\end{bmatrix} $$";
+" \\text{ y } X_2 =\\frac{1}{\\sqrt{5}} \\begin{bmatrix}  1 \\\\ 2 \\\\ \\end{bmatrix} $$";
 let expr7_6 = "$$A = O \\Lambda O^T \\text{ o } \\Lambda = O^T A O$$";
 
 let expr7_7="$$O=\\frac{1}{\\sqrt{5}} \\begin{bmatrix}  -2 & 1 \\\\ 1 & 2 \\\\ \\end{bmatrix}"+
 " \\text{ y } \\Lambda =\\begin{bmatrix}  1 & 0 \\\\ 0 & 6 \\\\ \\end{bmatrix} $$";
 
-let expr5_2="$[T]_{B}$";
-
-
-let expr6_1="$T(x,y,z) = (x+y,x-1)$";
-let expr6_2="$$\\begin{align*} T(1,0,0)=(1+0,1-0) &= (1,1)=1(1,0)+1(0,1) \\\\ T(0,1,0) = (0+1,0-0) &= (1,0) = 1(1,0)+0(0,1) \\\\"+
-            "T(0,0,1) = (0+0,0-1) &= (0,-1) = 0(1,0)-1(0,1) \\\\ \\end{align*}$$";
-let expr6_3="$$ [T]_{B,G}= \\begin{bmatrix}  1 & 1 & 0 \\\\" +
-"1 &  0 & -1 \\\\ \\end{bmatrix}$$";
-
-
-
-let expr9="$T: \\mathcal{P_2}(\\R) \\to \\R^3$";
-let expr9_1="$ [T]_{B,G}= \\begin{bmatrix}  1 & 1 & 2 \\\\" +
-"1 &  -1 & -0 \\\\ -1 & 1 & 2 \\\\ \\end{bmatrix}$";
-let expr9_2="$B=\\left\\{1,1+x,1+x^2\\right\\} \\text{ y } G=\\left\\{(1,1,0),(0,1,0),(0,0,1)\\right\\}$";
-
-let expr10="$$\\begin{align*} T(1)=1(1,1,0) + 1(0,1,0) - 1(0,0,1) &= (1,2,-1) \\\\ T(1+x)=1(1,1,0) - 1(0,1,0) + 1(0,0,1) &= (1,0,1) \\\\"+
-"T(1+x^2)=2(1,1,0) + 0(0,1,0) + 2(0,0,1) &= (2,2,2) \\\\ \\end{align*}$$";
-let expr10_1="$(1), (1+x) \\text{ y } (1+x^2)$";
-let expr10_2="$$\\begin{align*} a+bx+cx^2 &= \\alpha (1) + \\beta (1+x) + \\gamma (1+x^2) \\\\ a+bx+cx^2 &= \\alpha + \\beta + \\gamma + \\beta (x) + \\gamma (x^2) \\\\ \\end{align*}$$";
-let expr10_3="$$\\left\\{\\begin{matrix} \\alpha =a-b-c \\\\ \\beta =b \\\\ \\gamma =c \\\\ \\end{matrix}\\right.$$";
-let expr10_4="$$\\begin{align*} T(a+bx+cx^2) &= \\alpha T(1) + \\beta T(1+x) + \\gamma T(1+x^2) \\\\ T(a+bx+cx^2) &= (a-b-c)T(1)+bT(1+x)+cT(x^2) \\\\" +
-"T(a+bx+cx^2) &= (a-b-c)(1,2,-1)+b(1,0,1)+c(2,2,2) \\\\ \\end{align*}$$";
-let expr10_5="$$T(a+bx+cx^2) = (a+c,2a-2b,2b+3c-a)$$";
-
-let expr11="$T^{-1}$";
-let expr11_1="$$[T^{-1}]_{B,G}=[T]_{B,G}^{-1}$$";
-let expr11_2 = "$S:V \\to W$"
-let expr11_3="$$[T \\circ S]_{B,C}=[S]_{G,C} [T]_{B,G}$$";
-
+let expr8="$T \\in \\mathscr{L}(U)$";
+let expr8_1="$a_{i,j}=a_{ji},\\forall i,j$";
+let expr8_2="$A \in M_n(\\mathbb{C})$";
 const tema16 = () => {
     return (
         <Layout>
@@ -89,7 +62,51 @@ const tema16 = () => {
                     Las unicas matrices que pueden diagonalizarse ortogonalmente son las matrices simétricas debido a que la transpuesta de su matriz es igual a la inversa.
                 </p>
                 
+                <div className={`flex flex-row `}>
+                    <Image 
+                        src="/corolario_icon.png"
+                        alt="Corolario"
+                        width={60}
+                        height={60}
+                    />
+                    <h3 className={`mt-4 pb-4 `+ styles.defTitle}>Corolario:</h3>
+                </div>
+
                 
+                <p className={`leading-relaxed ` + styles.parrafo}>
+                    Sean <Latex>$V$</Latex> un espacio vectorial de dimensión finita y <Latex>$B$</Latex> una base ortonormal de <Latex>$V$</Latex>. si <Latex>{expr8}</Latex> es un
+                    operador lineal auto adjunto y si <Latex>{expr8_1}</Latex>. En particular los elementos de la diagonal de <Latex>$[T]_B$</Latex> sin números reales.
+
+                </p>
+
+                <div className={`flex flex-row `}>
+                    <Image 
+                        src="/corolario_icon.png"
+                        alt="Teorema"
+                        width={60}
+                        height={60}
+                    />
+                    <h3 className={`mt-4 pb-4 `+ styles.defTitle}>Teorema:</h3>
+                </div>
+
+                <p className={`leading-relaxed `+styles.parrafo}>
+                    Sea <Latex>$V$</Latex> un espacio vectorial complejo de dimensión finita con producto interno y <Latex>{expr8}</Latex> hermitiano. Entonces
+                    <Latex>$T$</Latex> es un operador diagonalizable. 
+                </p>
+                <div className={`flex flex-row `}>
+                    <Image 
+                        src="/proposicion_icon.png"
+                        alt="Proposición"
+                        width={60}
+                        height={60}
+                    />
+                    <h3 className={`mt-4 pb-4 `+ styles.defTitle}>Proposición:</h3>
+                </div>
+                <p className={`leading-relaxed `+styles.parrafo}>
+                    Sea <Latex>{expr8_2}</Latex> una matriz hermitiana. Entonces, <Latex>$A$</Latex> es una matriz diagonalizable, es decir,
+                    <Latex>$A$</Latex> es unitariamente semejante a una matriz diagonal.
+                </p>
+
                 <div className={`flex flex-row `}>
                     <Image 
                         src="/ejercicio_icon.png"
@@ -97,7 +114,7 @@ const tema16 = () => {
                         width={60}
                         height={60}
                     />
-                    <h3 className={`mt-4 pb-4 leading-relaxed `+ styles.defTitle}>Ejemplo 1:</h3>
+                    <h3 className={`mt-4 pb-4 leading-relaxed `+ styles.defTitle}>Ejemplo 2:</h3>
                 </div>
                 
                 <p className={`leading-relaxed ` + styles.parrafo}>
